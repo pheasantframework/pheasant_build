b5b0b70cf473136fb5ff70377e23b145a6d6314d
Nike <nikechukwu@gmail.com>
2024-03-28 16:04:42 -0400

    Code changes in pheasant_temp

--- lib/src/build/src/pheasant_file_builder.dart
+++ lib/src/build/src/pheasant_file_builder.dart
@@ -2,14 +2,14 @@ import 'dart:async' show FutureOr;
 
 import 'package:build/build.dart' show BuildStep, Builder;
 import 'package:path/path.dart' show basenameWithoutExtension, dirname;
-import 'package:pheasant_temp/pheasant_temp.dart' show renderFunc;
+import 'package:pheasant_temp/pheasant_temp.dart' show compilePhs;
 
 import '../../src/base.dart';
 import '../../tools/input.dart' hide PheasantInput;
 
 /// Builder Class used in building, processing and creating the dart-type Pheasant Files.
 ///
-/// This class is used in compiling Pheasant Files to dart-html component files to inject into the DOM, through the use of [renderFunc].
+/// This class is used in compiling Pheasant Files to dart-html component files to inject into the DOM, through the use of [compilePhs].
 class PheasantFileBuilder extends Builder {
   String fileExtension;
   bool js;
@@ -35,7 +35,7 @@ class PheasantFileBuilder extends Builder {
         renderInput(phsData: data, sassEnabled: sass);
     PheasantFile myIn = composedInput.input;
 
-    final dartCode = renderFunc(
+    final dartCode = compilePhs(
         script: myIn.script,
         template: myIn.template,
         componentName: "${filename}Component",
--- lib/src/src/render.dart
+++ lib/src/src/render.dart
@@ -9,17 +9,17 @@ import 'package:pheasant_temp/pheasant_build.dart'
 /// Whenever a change is emitted, the application is rerendered in the DOM.
 ///
 /// If [appState] is not provided, then the state is generated in the beginning of the app lifecycle.
-void _renderElement(PheasantTemplate app, {AppState? appState}) {
+void _renderElement(PheasantTemplate app, {AppState? appState, String? selector}) {
   AppState state = appState ??
       AppState(component: app, watchers: List.empty(growable: true));
   PheasantTemplate appObj = app;
   Element elementApp = appObj.render(appObj.template!, state);
-  querySelector('#output')?.children.add(elementApp);
+  querySelector(selector ?? '#output')?.children.add(elementApp);
 
   state.stateStream.listen(
     (event) {
       elementApp = appObj.render(appObj.template!, state);
-      querySelector('#output')?.children.first = elementApp;
+      querySelector(selector ?? '#output')?.children.first = elementApp;
       state.watchers
           .whereType<ElementChangeWatcher>()
           .forEach((element) => element.reflectChanges());
@@ -31,6 +31,6 @@ void _renderElement(PheasantTemplate app, {AppState? appState}) {
 ///
 /// The function accepts a pheasant file object - [PheasantTemplate] - and then processes the application.
 /// Once it is done, then it is injected into the DOM.
-void createApp(PheasantTemplate pheasantTemplate) {
-  _renderElement(pheasantTemplate);
+void createApp(PheasantTemplate pheasantTemplate, {AppState? appState, String? selector}) {
+  _renderElement(pheasantTemplate, appState: appState);
 }
