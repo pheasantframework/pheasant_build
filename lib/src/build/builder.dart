843eb2d513b8b00e92ef357dc685fd807cf185cf
Nike <nikechukwu@gmail.com>
2024-03-28 16:05:03 -0400

    New Builder

--- lib/src/build/builder.dart
+++ lib/src/build/builder.dart
@@ -1,9 +1,14 @@
 import 'package:build/build.dart' show Builder, BuilderOptions;
-import 'package:path/path.dart';
+import 'package:path/path.dart' show basenameWithoutExtension;
+
+import 'package:source_gen/source_gen.dart' show LibraryBuilder;
 
 import '../tools/config.dart' hide relativeFilePath;
+import 'src/pheasant_router_builder.dart' show PheasantRouterBuilder;
 import 'src/pheasant_main_builder.dart';
 import 'src/pheasant_file_builder.dart';
+
+Builder pheasantRouterBuilder(BuilderOptions builderOptions) => PheasantRouterBuilder();
 
 /// The Pheasant File Builder used to render Pheasant Files during build.
 Builder pheasantFileBuilder(BuilderOptions builderOptions) {
